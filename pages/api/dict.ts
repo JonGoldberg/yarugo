cd095ae38f1c7b98736f4d2dac828260e8c9db90
Jon Goldberg <jon.goldberg@yahoo.com>
2022-02-17 11:36:56 -0800

    Remove accidental api and page routes.

--- pages/api/dict.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiHandler } from "next";
-import words from "../../data/words/words.json";
-
-const dict: NextApiHandler = (req, res) => {
-  return res.status(200).json(words);
-}
-
-export default dict;
